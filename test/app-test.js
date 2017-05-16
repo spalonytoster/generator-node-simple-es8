35cb5fe86e7fbca54964cd3a4a46e2931f9152de
spalonytoster <xmachiei@gmail.com>
2017-05-16 16:07:40 +0200

    initial commit

---
+++ test/app-test.js
@@ -0,0 +1,12 @@
+'use strict';
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const app = require('../src/app.js');
+
+describe('App', () => {
+  it('Should...', () => {
+    let expected;
+    let result;
+    expect(result).to.be.equal(expected);
+  });
+});
